2fb9a1575dad36dfd29649ef61d041b2b1873a89
clairepender <clairepender5@gmail.com>
2021-10-02 14:11:14 -0500

    some cleanup

---
+++ src/components/Login.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react';
+
+
+const Login = () => {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Login; 
--- src/index.js
+++ src/index.js
@@ -2,40 +2,61 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import Login from './Login';
-import Posts from './Posts';
+import { 
+    Login, 
+    Posts 
+} from './Components';
+
+
 
 const App = () => {
-    const [posts, setPosts] = useState([]);
-    console.log('posts: ', posts);
+    //useState pairs here//
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch('https://strangers-things.herokuapp.com/api/2107-CSU-RM-WEB-PT/posts');
-            const data = await response.json();
-            setPosts(data.data.posts);
-            console.log('data: ', data.data.posts);
-        }
-        fetchPosts();
-    }, [])
 
-    return <>
-    <div>
-        Hello World
+    return <div className="app">
+        <Login />
+        <Posts />
     </div>
-
-    {
-        posts.map((post, idx) => <div key={idx}>
-            <h3>{post.title}</h3>
-            <p>Description: {post.description}</p>
-            <li>Price: {post.price}</li>
-            <li>Seller: {post.author.username}</li>
-        </div>)
-    }
-    </>
 }
 
 ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
+
+// const App = () => {
+//     const [posts, setPosts] = useState([]);
+//     console.log('posts: ', posts);
+
+//     useEffect(() => {
+//         const fetchPosts = async () => {
+//             const response = await fetch('https://strangers-things.herokuapp.com/api/2107-CSU-RM-WEB-PT/posts');
+//             const data = await response.json();
+//             setPosts(data.data.posts);
+//             console.log('data: ', data.data.posts);
+//         }
+//         fetchPosts();
+//     }, [])
+
+//     return <>
+//     <div>
+//         Hello World
+//     </div>
+
+//     {
+//         posts.map((post, idx) => <div key={idx}>
+//             <h3>{post.title}</h3>
+//             <p>Description: {post.description}</p>
+//             <li>Price: {post.price}</li>
+//             <li>Seller: {post.author.username}</li>
+//         </div>)
+//     }
+//     </>
+// }
+
+
+
+
+
+
+
