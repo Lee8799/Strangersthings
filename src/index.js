f45a74060d38ff62db62e8b3d4a422d2e4b4b4b0
clairepender <clairepender5@gmail.com>
2021-10-02 13:35:20 -0500

    added descriptions, prices, sellers to posts

---
+++ src/index.js
@@ -0,0 +1,38 @@
+
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    console.log('posts: ', posts);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('https://strangers-things.herokuapp.com/api/2107-CSU-RM-WEB-PT/posts');
+            const data = await response.json();
+            setPosts(data.data.posts);
+            console.log('data: ', data.data.posts);
+        }
+        fetchPosts();
+    }, [])
+
+    return <>
+    <div>
+        Hello World
+    </div>
+
+    {
+        posts.map((post, idx) => <div key={idx}>
+            <h3>{post.title}</h3>
+            <p>Description: {post.description}</p>
+            <li>Price: {post.price}</li>
+            <li>Seller: {post.author.username}</li>
+        </div>)
+    }
+    </>
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
